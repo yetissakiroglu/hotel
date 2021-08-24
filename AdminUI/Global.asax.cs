1107c4fceb2ef0e2190d05b05ef12f0bf5b29349
Yetiş Şakiroğlu <yetissakiroglu@gmail.com>
2021-08-24 15:06:26 +0300

    gunceleme yapıldı

---
+++ AdminUI/Global.asax.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
+using System.Reflection;
+
+namespace AdminUI
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterModule<FileManagerModule>();
+            builder.RegisterModule<FileSystemWrapperModule>();
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
